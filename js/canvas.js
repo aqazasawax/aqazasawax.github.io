06ee44207e15204e743fad8d31477d419c750e76
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-02-21 22:34:23 -0600

    Create canvas.js

---
+++ js/canvas.js
@@ -0,0 +1,22 @@
+var canvas;
+var ctx;
+
+window.addEventListener("resize", (_=>resizeCanvas()));
+
+function retrieveCanvasData() {
+	let treeCanv = document.getElementById("treeCanvas")
+	let treeTab = document.getElementById("treeTab")
+	if (treeCanv===undefined||treeCanv===null) return false;
+	if (treeTab===undefined||treeTab===null) return false;
+	canvas = treeCanv;
+	ctx = canvas.getContext("2d");
+	return true;
+}
+
+function resizeCanvas() {
+	if (!retrieveCanvasData()) return
+	canvas.width = 0;
+    canvas.height = 0;
+    canvas.width = document.getElementById("treeTab").scrollWidth;
+    canvas.height = document.getElementById("treeTab").scrollHeight;
+}
