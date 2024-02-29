4b394f0bea23c3ad296431843da57a9db60d8334
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-02-29 13:49:39 -0600

    Update upgrades.js

--- js/upgrades.js
+++ js/upgrades.js
@@ -4,6 +4,9 @@ const UPGRADES = {
                         desc: "Start your adventure as a warrior. Gain 1 point per second.",
                         cost: new Decimal(1),
                         costType: "points",
+                        unlocked() {
+
+                        },
                         active() {
                           
                         },
