0f9372b75122c534026f6b708ad8fd13bcb8f4af
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-02-29 12:17:42 -0600

    Update v.js

--- js/v.js
+++ js/v.js
@@ -4,11 +4,9 @@ function loadVue() {
         Vue.component('upgrade', {
                 props: ['index', 'tab'],
                 template: `
-                        <button v-if="onTab(tab)"
+                        <button 
                                 v-bind:id="upg"
-                                v-on:click="function() {
-                                        if (canAfford(index.costType, tab, index)) index.purchased = true;
-                                }">
+                                >
                                 description
                         </button>
                 `
