905c3aae600b81dc4adec7d97e4134a29fa7c611
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-03-05 09:30:22 -0600

    Update v.js

--- js/v.js
+++ js/v.js
@@ -1,1 +1,20 @@
+var app;
 
+function loadVue() {
+  Vue.component('upgrade-button', {
+    props: ['id', 'desc'],
+    template: `
+    <button>
+    {{desc}}
+    </button>
+    `
+  })
+
+  app = new Vue({
+    el: "#app",
+    data: {
+      player,
+      UPGRADES
+    },
+  })
+}
