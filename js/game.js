86d1fb871ea11be8befbb443027a3ab9edaf9f5b
aqazasawax <130019068+aqazasawax@users.noreply.github.com>
2024-03-05 09:44:58 -0600

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,1 +1,41 @@
+var player;
 
+function startPlayer() {
+  return {
+    time: Date.now(),
+    timePlayed: 0,
+    points: new Decimal(10),
+  }
+}
+
+function save() {
+  localStorage.setItem("save-data", btoa(JSON.stringify(player)))
+}
+
+function load() {
+  let get = localStorage.getItem("save-data");
+  if (get === null||get === undefined) player = startPlayer()
+  else player = JSON.parse(atob(get))
+  player.time = Date.now()
+  loadVue();
+}
+
+function addTime(diff) {
+  let data = player
+  let time = data.timePlayed
+  time += toNumber(diff)
+  data.timePlayed = time
+}
+
+function gameLoop(diff) {
+  addTime(diff)
+  if (diff === 0) return
+}
+
+var interval = setInterval(function() {
+  if (player === undefined) return;
+  let now = Date.now()
+  let diff = (now - player.time) / 1e3
+  player.time = now
+  gameLoop(diff)
+}, 50)
